a6aa9ea8a4c2ad5390cc0d211a3e0cf4882e668f
tatestill <tatestill@yahoo.com>
2025-04-27 19:58:02 -0400

    First 2D Movement
    Implements start of 2D movement, including groundwork for states on player character.

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,12 +1,63 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed: int = 400
+@export var acceleration: int = 35
+@export var gravity: int = speed * 5
+@export var down_gravity_factor: float = 3
+@export var jump_speed: int = -speed * 2
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	var gravity = Vector2(0,50)
-	velocity = (input_direction * speed) + gravity
-	
+
+@onready var jump_buffer_timer: Timer = $JumpBufferTimer
+@onready var coyote_timer: Timer = $CoyoteTimer
+
+enum State{IDLE, WALK, JUMP, DOWN}
+var current_state: State = State.IDLE
+
 func _physics_process(delta):
 	get_input()
+	update_movement(delta)
+	update_states()
 	move_and_slide()
+	
+	
+func get_input():
+	if (Input.is_action_just_pressed("up")):
+		jump_buffer_timer.start()
+
+	var direction = Input.get_axis("left", "right")
+	if direction == 0:
+		velocity.x = move_toward(velocity.x, 0, acceleration)
+	else:
+		velocity.x = move_toward(velocity.x, speed*direction, acceleration)
+	
+func update_movement(delta: float) -> void:
+	if (is_on_floor() || coyote_timer.time_left >0) && jump_buffer_timer.time_left > 0:
+		velocity.y = jump_speed
+		current_state = State.JUMP
+		jump_buffer_timer.stop()
+		coyote_timer.stop()
+		
+	if current_state == State.JUMP:
+		velocity.y += gravity * delta
+	else:
+		velocity.y += gravity * delta * down_gravity_factor
+	
+func update_states() -> void:
+	match current_state:
+		State.IDLE when velocity.x != 0:
+			current_state = State.WALK
+			
+		State.WALK:
+			if velocity.x == 0:
+				current_state = State.IDLE
+				if not is_on_floor() && velocity.y > 0:
+					current_state = State.DOWN
+					coyote_timer.start()
+		State.JUMP when velocity.y > 0:
+			current_state = State.DOWN
+		
+		State.DOWN when is_on_floor:
+			if velocity.x == 0:
+				current_state = State.IDLE
+			else:
+				current_state = State.WALK
